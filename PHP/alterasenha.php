0f9d375b26972eb66a98aca6b6c54238457348bf
luiz-matosinho <luizfematosinho@gmail.com>
2023-10-17 08:44:23 -0300

    Alterarsenha completo

--- PHP/alterasenha.php
+++ PHP/alterasenha.php
@@ -5,24 +5,15 @@ include "caixa.php";
 $conn = coneccao();
 
 $email = $_GET['email'];
-
-
+echo $email;
 
 echo"
-<form action='' method='post'>
+<form action='updatesenha.php' method='post'>
+    <input type='text' value='$email' name='email'>
     <input type='text' name='novasenha' placeholder='digite a nova senha...'>
     <input type='submit'>
 </form>
 ";
 
 
-if(isset($_POST)){
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-       $novasenha = $_POST['novasenha'];
-        echo"$novasenha";
-       $sql2 = "update tbl_usuario set senha = $novasenha where email = $email";
-       $atualiza = $conn->prepare($sql2);
-       $atualiza->execute();
-    }
- }
 ?>
