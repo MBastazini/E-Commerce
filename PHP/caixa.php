9cd1a0f61b3cab54150af657be28517ce59dd7c4
luiz-matosinho <luizfematosinho@gmail.com>
2023-10-20 07:58:26 -0300

    pdf final

--- PHP/caixa.php
+++ PHP/caixa.php
@@ -350,60 +350,106 @@
   }
 
 
-  use Dompdf\Dompdf;
-
-  function gerapdf($html){
-
-    require "dompdf/autoload.inc.php";
-    require "dompdf/polyfill-mbstring-1.x/Mbstring.php";
-    
-    // instantiate and use the dompdf class
-    $dompdf = new Dompdf();
-    $dompdf->loadHtml($html);
-
-    // (Optional) Setup the paper size and orientation
-    $dompdf->setPaper('A4', 'landscape');
-
-    // Render the HTML as PDF
-    $dompdf->render();
-
-    // Output the generated PDF to Browser
-    $dompdf->stream();
-  }
-
-  /*
-  fpdf----------------
   function gerapdf($html){
     require('FPDF/fpdf.php');
 
-	$pdf = new FPDF();
-	$pdf->AddPage();
-	$pdf->SetFont('Arial','',12);
-    /*Cell(float w [, float h [, string txt [, mixed border [, int ln [, string align [, boolean fill [, mixed link]]]]]]])
-    w->width (if 0 extends up to the right margin)
-    h-> height
-    txt -> text
-    border -> 0: no border
-                1: frame
-                L: left
-                T: top
-                R: right
-                B: bottom
-    ln ->Indicates where the current position should go after the call. Possible values are:
-        0: to the right
-        1: to the beginning of the next line
-        2: below
-        Putting 1 is equivalent to putting 0 and calling Ln() just after. Default value: 0.
-    align -> L or empty string: left align (default value)
-            C: center
-            R: right align
-    fill-> Indicates if the cell background must be painted (true) or transparent (false). Default value: false.
-    link -> URL or identifier returned by AddLink().
- 
-	$pdf->Cell(0,10,$html);
-	$pdf->Output();
+    class pdf extends FPDF
+        {
+        protected $B = 0;
+        protected $I = 0;
+        protected $U = 0;
+        protected $HREF = '';
+
+        function WriteHTML($html)
+        {
+            // HTML parser
+            $html = str_replace("\n",' ',$html);
+            $a = preg_split('/<(.*)>/U',$html,-1,PREG_SPLIT_DELIM_CAPTURE);
+            foreach($a as $i=>$e)
+            {
+                if($i%2==0)
+                {
+                    // Text
+                    if($this->HREF)
+                        $this->PutLink($this->HREF,$e);
+                    else
+                        $this->Write(5,$e);
+                }
+                else
+                {
+                    // Tag
+                    if($e[0]=='/')
+                        $this->CloseTag(strtoupper(substr($e,1)));
+                    else
+                    {
+                        // Extract attributes
+                        $a2 = explode(' ',$e);
+                        $tag = strtoupper(array_shift($a2));
+                        $attr = array();
+                        foreach($a2 as $v)
+                        {
+                            if(preg_match('/([^=]*)=["\']?([^"\']*)/',$v,$a3))
+                                $attr[strtoupper($a3[1])] = $a3[2];
+                        }
+                        $this->OpenTag($tag,$attr);
+                    }
+                }
+            }
+        }
+
+        function OpenTag($tag, $attr)
+        {
+            // Opening tag
+            if($tag=='B' || $tag=='I' || $tag=='U')
+                $this->SetStyle($tag,true);
+            if($tag=='A')
+                $this->HREF = $attr['HREF'];
+            if($tag=='BR')
+                $this->Ln(5);
+        }
+
+        function CloseTag($tag)
+        {
+            // Closing tag
+            if($tag=='B' || $tag=='I' || $tag=='U')
+                $this->SetStyle($tag,false);
+            if($tag=='A')
+                $this->HREF = '';
+        }
+
+        function SetStyle($tag, $enable)
+        {
+            // Modify style and select corresponding font
+            $this->$tag += ($enable ? 1 : -1);
+            $style = '';
+            foreach(array('B', 'I', 'U') as $s)
+            {
+                if($this->$s>0)
+                    $style .= $s;
+            }
+            $this->SetFont('',$style);
+        }
+
+        function PutLink($URL, $txt)
+        {
+            // Put a hyperlink
+            $this->SetTextColor(0,0,255);
+            $this->SetStyle('U',true);
+            $this->Write(5,$txt,$URL);
+            $this->SetStyle('U',false);
+            $this->SetTextColor(0);
+        }
+        }
+        
+    $pdf = new pdf();
+    $pdf->AddPage();
+    $pdf->SetFont('Arial','',20);
+    //$pdf->SetLeftMargin(45);
+    $pdf->SetFontSize(12);
+    $pdf->WriteHTML($html);
+    $pdf->Output();
   }
-    */
+    
 
   
 
